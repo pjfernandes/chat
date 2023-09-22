ca64f1d9161cd9aebdc86f7d91c97be74cdbf7f0
pfernandes <pj_fernandes@id.uff.br>
2023-09-22 18:55:45 -0300

    done

--- lib/core/services/auth/auth_service.dart
+++ lib/core/services/auth/auth_service.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:chat/core/models/chat_user.dart';
-import 'package:chat/core/services/auth/auth_mock_service.dart';
+import 'package:chat/core/services/auth/auth_firebase_service.dart';
 
 abstract class AuthService {
   ChatUser? get currentUser;
